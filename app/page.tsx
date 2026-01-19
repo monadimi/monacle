d8b3380602c36bed11756760b94ae473f7fb1848
Choi Yoon-hyuk <dbsgur123321@gmail.com>
2026-01-19 16:57:08 +0900

    fix: redirect address error

--- app/page.tsx
+++ app/page.tsx
@@ -25,7 +25,7 @@ export default function LoginPage() {
     // 3. Redirect to Monad ID
     const params = new URLSearchParams({
       client_id: CLIENT_ID,
-      redirect_uri: "http://localhost:3000/callback",
+      redirect_uri: "https://monacle.snowman0919.site/callback",
       response_type: "code",
       scope: "email name type",
       state: state,
